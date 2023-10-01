b72792e63186f7ffafa769f94d944413aee35866
Dale Barriga <127873937+reyylaxed@users.noreply.github.com>
2023-10-01 15:45:42 -0600

    initial commit- week 5

---
+++ Student/Week-05/Superhero/hero/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Superhero(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.CharField(max_length=200)
