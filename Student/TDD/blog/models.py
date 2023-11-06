7f7496a2a89c747512ac932652126f3191d0f9ba
Dale Barriga <127873937+reyylaxed@users.noreply.github.com>
2023-11-05 18:23:50 -0700

    week 8

--- Student/TDD/blog/models.py
+++ Student/TDD/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.urls import reverse_lazy
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse_lazy("article_list")
